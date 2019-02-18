31b744834aeecf51fa2f0fe1eeaa3be533b126dc
pablo a maurin <pablo@maurin.org>
2019-02-18 10:40:26 -0700

    Add more details on step to prepare for dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,17 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 # install powerline9k
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
+# set .zshrc THEME to
+ZSH_THEME="powerlevel9k/powerlevel9k"
+
+#install powerline fonts
+git clone htddtps://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+
+# install nerd fonts
+git clone -o ryanoasis git@github.com:ryanoasis/nerd-fonts.git
+cd nerd-fonts
+./install.sh
+
+# Select font with glyphs in iTerm2
