39ee17a6f6b9c2d046884a96fd9df7b1b9da178f
pablo a maurin <pablo@maurin.org>
2019-02-17 21:43:47 -0700

    Begining of boothstrap scripts

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+# check stow is available
+zsh is intalled and current shell
+
+#install oh-my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# install powerline9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
